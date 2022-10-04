17dbee505f00e393cc215a371623b72f8445dfef
unknown <70929180+jasonliny@users.noreply.github.com>
2022-10-04 08:29:13 -0400

    Setup react router and do more cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+const router = createBrowserRouter({
+  path: "/",
+  element: <div>Hello World!</div>,
+});
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
